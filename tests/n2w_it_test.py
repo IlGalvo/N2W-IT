0efa43a2c63278576ce73146e96c6ddad9279b4f
Andrea Galvani <17992288+IlGalvo@users.noreply.github.com>
2021-03-27 20:38:13 +0100

    Updated package and tests.

--- tests/n2w_it_test.py
+++ tests/n2w_it_test.py
@@ -3,33 +3,32 @@ from n2w_it import N2W_IT
 
 
 class Test_N2W_IT(unittest.TestCase):
-    """Tests for `n2w_it` package."""
-
     def setUp(self):
-        """Set up test fixtures, if any."""
-
         self._instance = N2W_IT()
 
     def test_int_to_cardinal(self):
-        """Test something."""
+        value1 = self._instance.int_to_cardinal(987654321063)
+        value2 = "novecentottantasette miliardi, seicentocinquantaquattro milioni e trecentoventunomilasessantatré"
 
-        self.assertEqual(self._instance.int_to_cardinal(5), "cinque")
+        self.assertEqual(value1, value2)
 
     def test_float_to_cardinal(self):
-        """Test something."""
+        value1 = self._instance.float_to_cardinal(156.0023868)
+        value2 = "centocinquantasei virgola zero zero ventitremilaottocentosessantotto"
 
-        self.assertEqual(self._instance.float_to_cardinal(
-            5.5), "cinque virgola cinque")
+        self.assertEqual(value1, value2)
 
     def test_int_to_ordinal(self):
-        """Test something."""
+        value1 = self._instance.int_to_ordinal(137)
+        value2 = "centotrentasettesimo"
 
-        self.assertEqual(self._instance.int_to_ordinal(5), "quinto")
+        self.assertEqual(value1, value2)
 
     def test_roman_to_ordinal(self):
-        """Test something."""
+        value1 = self._instance.roman_to_ordinal("DCLXII")
+        value2 = "seicentosessantaduesimo"
 
-        self.assertEqual(self._instance.roman_to_ordinal("V"), "quinto")
+        self.assertEqual(value1, value2)
 
 
 if __name__ == "__main__":
